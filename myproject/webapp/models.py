a24993091ade5de9b6224e87b03a71f94fdadcb6
davidkorea <lyfirst@naver.com>
2018-02-09 17:20:59 +0900

    change models.py choices of charfield

--- myproject/webapp/models.py
+++ myproject/webapp/models.py
@@ -7,8 +7,13 @@ class Caselog(models.Model):
     orgnization = models.CharField(null=True, blank=True,max_length=200)
     problem = models.CharField(null=True, blank=True,max_length=200)
     solution = models.TextField(null=True, blank=True)
-    handleby = models.CharField(max_length=100, default='',
-                                choices=((u'Andy',u'Andy'),(u'David',u'David')))
+    # handleby = models.CharField(max_length=100, default='',
+    #                             choices=((u'Andy',u'Andy'),(u'David',u'David')))
+    MEMBER = (
+        ('david','David'),
+        ('andy','Andy'),
+    )
+    handleby = models.CharField(null=True, blank=True, max_length=5, choices=MEMBER)
     date = models.DateField(null=True)
     def __str__(self):
         return self.caseid
