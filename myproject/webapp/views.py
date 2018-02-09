f909ee760e7ccf32d8e9e11f0d6451555389ac26
davidkorea <lyfirst@naver.com>
2018-02-09 17:44:25 +0900

    sort data by request.get

--- myproject/webapp/views.py
+++ myproject/webapp/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from webapp.models import Caselog
 
 def default(request):
+    print('==='*30)
+    print(request)
+    print('==='*30)
+    print(dir(request))
+    print('==='*30)
+    print(type(request))
+    print('==='*30)
+    queryset = request.GET.get('handleby')
+    print(queryset)
+    if queryset:
+        caselog_list = Caselog.objects.filter(handleby=queryset)
+    else:
+        caselog_list = Caselog.objects.all()
     context = {
-        'Caselog':Caselog.objects.all()
+        'Caselog': caselog_list
     }
     return render(request, 'default.html', context)
