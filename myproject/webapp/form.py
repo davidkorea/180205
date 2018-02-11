d2fa7a3701fce140cfba992ec8ac4dd008a38e44
davidkorea <lyfirst@naver.com>
2018-02-11 23:50:04 +0900

    add validations in form.py

--- myproject/webapp/form.py
+++ myproject/webapp/form.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+def words_validator(comment):
+    if len(comment) < 4:
+        raise ValidationError('Not Enough Letters')
+
+def comment_validator(comment):
+    if 'a' in comment:
+        raise ValidationError('Do Not Use the Word!')
 
 class CommentForm(forms.Form):
     name = forms.CharField(max_length=50)
-    comment = forms.CharField()
+    comment = forms.CharField(
+        widget=forms.Textarea(),
+        error_messages={
+            'required': 'why no words?'
+        },
+        validators=[words_validator, comment_validator]
+                              )
