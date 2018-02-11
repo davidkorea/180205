ce2e9e9f7562e7f52f98c4a684bcfc789c69f471
davidkorea <lyfirst@naver.com>
2018-02-11 22:41:29 +0900

    create commentform class in views.py

--- myproject/webapp/views.py
+++ myproject/webapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from webapp.models import Caselog, Comment
+from django import forms
+
+class CommentForm(forms.Form):
+    name = forms.CharField(max_length=50)
+    comment = forms.CharField()
 
 def default(request):
     print('==='*30)
@@ -21,7 +26,9 @@ def default(request):
     return render(request, 'default.html', context)
 
 def detail(request):
+    form = CommentForm
     context = {}
     comment_list = Comment.objects.all()
     context['comment_list'] = comment_list
+    context['form'] = form
     return render(request, 'log_detail.html', context)
